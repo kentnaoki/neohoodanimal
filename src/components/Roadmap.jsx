17276a5c85f32aa78fc7b45510261011d1afccc8
naoki <ueda.191201@gmail.com>
2022-12-28 07:03:47 +0000

    briefly completed the website

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,10 +1,35 @@
 import styled from "styled-components";
+import cat from "../img/test_image.jfif";
 
 const RoadmapStyle = styled.div`
+  padding-top: 65px;
+  padding-bottom: 65px;
+
   .roadmap-content {
     display: grid;
     grid-template-rows: 50% 50%;
     grid-template-columns: 50% 50%;
+    max-width: 940px;
+    margin: 0 auto;
+  }
+  .roadmap-item {
+    display: flex;
+    margin: 0 10px;
+  }
+  .roadmap-image {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .roadmap-image img {
+    max-width: 130px;
+    height: auto;
+  }
+  .roadmap-text {
+    margin: 0 10px;
+  }
+  .roadmap-paragraph {
+    text-align: justify;
   }
 `;
 
@@ -15,35 +40,44 @@ export const Roadmap = () => {
       header:
         "本IPに対する無制限の商業利用を許可します（Permission for unrestricted commercial use of this IP）",
       text: "あなたの所有するNFTにはIP利用の権利を所有しております。",
+      image: cat,
     },
     {
       id: 1,
       header: "アクセス制限（exclusive access）",
-      text: "あなたのNFTは食べのミクスでの体験へのアクセスを許可しています",
+      text: "あなたのNFTは食べのミクスでの体験へのアクセスを許可しています。",
+      image: cat,
     },
     {
       id: 2,
       header: "著名人が集まるオフ会（Off-line meetings with celebrities）",
-      text: "あなたは日本のVIPが集まる会に参加する権利を持っています（You have the right to attend a gathering of Japanese VIPs）",
+      text: "あなたは日本のVIPが集まる会に参加する権利を持っています。（You have the right to attend a gathering of Japanese VIPs）",
+      image: cat,
     },
     {
       id: 3,
       header:
         "日本の有名飲食店のWeb3化支援（Web3 support for famous Japanese restaurants）",
-      text: "我々は既存の飲食業界をWeb3領域まで引き上げることをミッションにしています",
+      text: "我々は既存の飲食業界をWeb3領域まで引き上げることをミッションにしています。",
+      image: cat,
     },
   ];
 
   return (
     <RoadmapStyle>
-      <div>
+      <div id="roadmap">
         <h2>ROADMAP</h2>
         <div className="roadmap-content">
           {roadmapContent.map((element) => {
             return (
               <div className="roadmap-item" key={element.id}>
-                <h3 className="roadmap-header">{element.header}</h3>
-                <p className="roadmap-text">{element.text}</p>
+                <div className="roadmap-image">
+                  <img src={element.image} alt={element.header} />
+                </div>
+                <div className="roadmap-text">
+                  <h4 className="roadmap-header">{element.header}</h4>
+                  <p className="roadmap-paragraph">{element.text}</p>
+                </div>
               </div>
             );
           })}
--- src/components/Statement.jsx
+++ src/components/Statement.jsx
@@ -1,19 +1,36 @@
+import styled from "styled-components";
+
+const StatementStyle = styled.div`
+  background-color: #383838;
+  padding-top: 65px;
+  padding-bottom: 65px;
+  .statement-content {
+    max-width: 940px;
+    margin: 0 auto;
+  }
+  p {
+    text-align: justify;
+  }
+`;
+
 export const Statement = () => {
   return (
-    <div>
-      <h2>STATEMENT</h2>
-      <h3>
-        We will update the existing food and beverage industry with Web3.and
-        give back to Japan
-      </h3>
-      <p>
-        Hikomaro is Legendary food-reporting comedian. He handles Web 3 for the
-        food and beverage industry.NEO HOOD ANIMAL are not just PFPs.This NFT
-        offers special benefits when multiple this NFT are collected and
-        destroyed And this NFT is also a special food community membership.We
-        are going to use Web3 to reform the restaurant industry with our
-        wonderful friends with this NFT.
-      </p>
-    </div>
+    <StatementStyle>
+      <div id="statement" className="statement-content">
+        <h2>STATEMENT</h2>
+        <h3>
+          We will update the existing food and beverage industry with Web3.and
+          give back to Japan
+        </h3>
+        <p>
+          Hikomaro is Legendary food-reporting comedian. He handles Web 3 for
+          the food and beverage industry.NEO HOOD ANIMAL are not just PFPs.This
+          NFT offers special benefits when multiple this NFT are collected and
+          destroyed And this NFT is also a special food community membership.We
+          are going to use Web3 to reform the restaurant industry with our
+          wonderful friends with this NFT.
+        </p>
+      </div>
+    </StatementStyle>
   );
 };
