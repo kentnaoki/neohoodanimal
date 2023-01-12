a563143d8c0dfb7d08a4e55e1a08d101824ffa33
naoki <ueda.191201@gmail.com>
2023-01-12 04:17:52 +0000

    moved helmet to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import metadata from "./metadata.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const data = metadata.data.info;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
+      <Helmet
+        title="NEO HOOD ANIMAL"
+        meta={[
+          { name: "description", content: data.description },
+          { property: "og:url", content: data.url },
+          { property: "og:type", content: "website" },
+          { property: "og:title", content: "NEO HOOD ANIMAL" },
+          { property: "og:image", content: data.opgImg },
+          {
+            property: "og:description",
+            content: data.description,
+          },
+          { name: "twitter:card", content: "summary" },
+          { name: "twitter:site", content: "@Tabenomics_web3" },
+          { name: "twitter:title", content: "NEO HOOD ANIMAL" },
+          { name: "twitter:description", content: data.description },
+          { name: "twitter:image", content: data.twitterImage },
+        ]}
+      />
+      <App />
+    </React.StrictMode>
+  </HelmetProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
