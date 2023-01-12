f4b887d849a29518fd305fcf52b5ad7c709d6b5b
naoki <ueda.191201@gmail.com>
2023-01-12 02:41:33 +0000

    added ogp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import metadata from "./metadata.json";
 import { Mint } from "./components/Mint";
 import { MintJP } from "./components/MintJP";
 import { Home } from "./components/Home";
@@ -12,20 +14,37 @@ const AppStyle = styled.div`
   /* font: "Sequel Sans Display Medium"; */
 `;
 
+const data = metadata.data.info;
+
 function App() {
   return (
-    <AppStyle>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path={`/mint/`} element={<Mint />} />
-            <Route path={`/jp/`} element={<HomeJP />} />
-            <Route path={`/jp/mint/`} element={<MintJP />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </AppStyle>
+    <HelmetProvider>
+      <AppStyle>
+        <div className="App">
+          <Helmet
+            title="NEO HOOD ANIMAL"
+            meta={[
+              { property: "og:url", content: data.url },
+              { property: "og:type", content: "website" },
+              { property: "og:titel", content: "NEO HOOD ANIMAL" },
+              { property: "og:image", content: data.opgImg },
+              {
+                property: "og:description",
+                content: data.description,
+              },
+            ]}
+          />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path={`/mint/`} element={<Mint />} />
+              <Route path={`/jp/`} element={<HomeJP />} />
+              <Route path={`/jp/mint/`} element={<MintJP />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </AppStyle>
+    </HelmetProvider>
   );
 }
 
